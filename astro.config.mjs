2e8600c06dcf2a3b0be87bf61352d246305c0edf
Crussus1719 <90951557+Crussus1719@users.noreply.github.com>
2026-05-16 02:55:47 -0300

    config: update site URL to actual Netlify domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://tech-blog-premium.netlify.app/',
+  site: 'https://glittery-sprinkles-ab77a4.netlify.app/',
   integrations: [tailwind()],
 });
