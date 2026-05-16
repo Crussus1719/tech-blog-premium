e14c3b9bc078a4ce4c59f53f138580566321f37f
Crussus1719 <90951557+Crussus1719@users.noreply.github.com>
2026-05-16 03:17:11 -0300

    Feat: Add Cookie Banner, update site URL, and add About link to navigation. LGPD compliance and SEO fixes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://glittery-sprinkles-ab77a4.netlify.app/',
-  integrations: [tailwind()],
+  site: 'https://glittery-sprinkles-ab77a4.netlify.app',
+  integrations: [tailwind(), sitemap()],
 });
