3135b46b3c8b320d863eb128836ca0cb95f149c0
Crussus1719 <90951557+Crussus1719@users.noreply.github.com>
2026-05-16 02:44:51 -0300

    fix: remove sitemap integration temporarily to bypass build error 'reduce of undefined'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tech-blog-premium.netlify.app/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
